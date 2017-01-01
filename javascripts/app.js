6b4615a46c1ef6dc38ac4ff334560605a5ca282e
aaronistheman <aarons.7007@gmail.com>
2016-12-31 23:15:31 -0800

    AngularJS is used to make generating the project preview panels easier

--- javascripts/app.js
+++ javascripts/app.js
@@ -37,10 +37,59 @@ function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('home');
 }]);
 
+
+
+/**
+ * Custom type for storing data to be displayed for a specific project
+ * preview panel.
+ */
+function ProjectPanelData(name, dateRangeStr, programmingLanguagesStr,
+	testingFramework, otherLanguagesOrSoftwareStr, githubLink)
+{
+	this.name = name;
+	this.dateRangeStr = dateRangeStr;
+	this.programmingLanguagesStr = programmingLanguagesStr;
+	this.testingFramework = testingFramework;
+	this.otherLanguagesOrSoftwareStr = otherLanguagesOrSoftwareStr;
+	this.githubLink = githubLink;
+}
+
+function getProjectPanelData()
+{
+	var projectPanelData = [];
+
+	// The first data pushed in is displayed first
+	projectPanelData.push(new ProjectPanelData(
+		"2D Square Cannonade Desktop Game",
+		"September 2016 - Current",
+		"C++",
+		"Catch",
+		"SFML, Git",
+		"https://github.com/aaronistheman/SFML-Square-Cannonade-Game/tree/develop"));
+	projectPanelData.push(new ProjectPanelData(
+		"Simplified Economic Model",
+		"June - September 2016",
+		"JavaScript",
+		"QUnit",
+		"HTML/CSS, Git",
+		"https://github.com/aaronistheman/Supply-Demand-Graph-Calculations"));
+	projectPanelData.push(new ProjectPanelData(
+		"2D Spacecraft Android Game",
+		"December 2015 - April 2016",
+		"Java",
+		"JUnit and Android extensions",
+		"Android Studio, Autodesk Inventor, Git",
+		"https://github.com/aaronistheman/Android-Spacecraft-Game"));
+
+	return projectPanelData;
+}
+
+
+
 app.controller('MainCtrl', [
 '$scope',
 function($scope) {
-
+	$scope.projectPanelData = getProjectPanelData();
 }]);
 
 app.controller('EducationCtrl', [
